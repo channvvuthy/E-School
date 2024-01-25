f8f32dfd876434939e5f3112ba3d3119034e6f4e
channvvuthy <vuthychan271@yahoo.com>
2024-01-25 15:31:04 +0700

    Remove method

--- lib/utils/helper/validation.dart
+++ lib/utils/helper/validation.dart
@@ -57,13 +57,6 @@ class Validation {
     }
   }
 
-  static String capitalize(String text) {
-    if (text.isEmpty) {
-      return text;
-    }
-    return text[0].toUpperCase() + text.substring(1);
-  }
-
   static makeMessage(Map<String, List<String>> errors) {
     if (errors.isNotEmpty) {
       Get.snackbar(
